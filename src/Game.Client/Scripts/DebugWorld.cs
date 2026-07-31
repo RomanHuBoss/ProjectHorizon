f7d35d725fb8aaf94081046be979a9ce0a3f6e6b
roman <roman@web-line.ru>
2026-08-01 00:21:57 +0300

    Add debug & change driver.windows

--- src/Game.Client/Scripts/DebugWorld.cs
+++ src/Game.Client/Scripts/DebugWorld.cs
@@ -5,5 +5,7 @@ public partial class DebugWorld : Node3D
     public override void _Ready()
     {
         GD.Print("Project Horizon: DebugWorld успешно запущен.");
+        GD.Print($"Rendering method: {RenderingServer.GetCurrentRenderingMethod()}");
+        GD.Print($"Rendering driver: {RenderingServer.GetCurrentRenderingDriverName()}");
     }
 }
